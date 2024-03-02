ba38b830e578c449b84079af9db981fb5f09b9d7
Daniel R <nzdanron@gmail.com>
2024-03-03 09:45:46 +1300

    feat: Add meme fetching
    Implemented an mvp of meme fetching. The API need's cleaning up, and docs need adding

--- meme-vault-fetcher/src/lib.rs
+++ meme-vault-fetcher/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    fs::File,
+    io::{copy, Write},
+    path::Path,
+};
+
+const API_URL: &str = "https://meme-api.com/gimme/";
+
+const SUBREDDITS: [&str; 2] = ["ProgrammerHumor", "WholesomeMemes"];
+
+#[derive(Clone, Copy)]
+pub enum MemeSource {
+    ProgrammerHumor,
+    WholesomeMemes,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ToString for MemeSource {
+    fn to_string(&self) -> String {
+        SUBREDDITS[*self as usize].to_string()
     }
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Meme {
+    title: String,
+    url: String,
+}
+
+pub fn get_meme(source: MemeSource) -> Result<Meme, reqwest::Error> {
+    let url = format!("{}{}", API_URL, source.to_string());
+
+    let response = reqwest::blocking::get(url)?;
+
+    let meme: Meme = response.json()?;
+
+    Ok(meme)
+}
+
+pub fn download_meme(meme: &Meme, dest_dir: &str) -> Result<(), reqwest::Error> {
+    let path = format!("{}/{}.png", dest_dir, meme.title);
+
+    let mut dest = File::create(&path).expect("Failed to create file");
+
+    let response = reqwest::blocking::get(meme.url.as_str())?
+        .copy_to(&mut dest)
+        .unwrap();
+
+    Ok(())
 }
